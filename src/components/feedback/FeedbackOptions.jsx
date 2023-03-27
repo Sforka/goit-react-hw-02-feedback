69f7b30ecbc0c977d7f33e581c5cf492d9c69456
Sforka <warlock20115@gmail.com>
2023-03-27 16:33:35 +0300

    1
    1

--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -19,6 +19,6 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.array.isRequired
-  
-}
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.array.isRequired,
+};
